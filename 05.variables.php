454cb7f1a7d4e35ffd0a160d3d06cf61fe248acf
Shubhamty8586 <98882978+Shubhamty8586@users.noreply.github.com>
2023-07-24 17:52:47 +0530

    Add files via upload

---
+++ 05.variables.php
@@ -0,0 +1,10 @@
+<?php 
+//echo "This is 5 tutorial";
+// variables are containers for storing information
+// Starts with a $
+$name = "Shubham";
+$income = 200;
+echo "This guy is $name and his income is Rs. $income <br>";
+echo "$name is a good boy <br>";
+echo "$name is a real gangster";
+?>
