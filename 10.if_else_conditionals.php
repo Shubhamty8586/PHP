fb1c77298af75e5498128942b3bc67a5e20e24b4
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-07-28 10:09:25 +0530

    Add files via upload

---
+++ 10.if_else_conditionals.php
@@ -0,0 +1,33 @@
+<?php
+$a = 665;
+$b =20;
+if($a > 78){
+    echo "a is grater than 78";
+}
+else {
+    echo "a is not grater than 78";
+}
+
+$age = 24;
+if ($age > 18){
+echo "You can drink Chai and alchol";
+}
+elseif ($age > 13){
+    echo "You can drink Chai and water only.No Alchol";
+    }
+else {
+    echo "You can drink water only";
+    }
+
+if ($age > 18){
+        echo "You can drink Chai and alchol<br>";
+    }
+if ($age > 13){
+            echo "You can drink Chai and water only.No Alchol<br>";
+    }
+else {
+       echo "You can drink water only<br>";
+    }
+echo  "Done";      
+
+?>
