38aa267ef744db05996fb635281116c476b2d151
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-07-29 10:59:38 +0530

    Add files via upload

---
+++ 11.Switch_case.php
@@ -0,0 +1,17 @@
+<?php
+$age = 12;
+switch($age){
+    case 12:
+    echo "You are 12 years Old";
+    break;
+    case 45:
+    echo "You are 45 years Old";
+    break;
+    case 56:
+    echo "You are 56 years Old";
+    break;
+    default:
+    echo "You Are Weired";
+    break;
+}
+?>
