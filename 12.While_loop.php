618c01f9ae4e16fac0deeef8feb34925bd466d53
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-07-30 23:31:28 +0530

    Add files via upload

---
+++ 12.While_loop.php
@@ -0,0 +1,61 @@
+<?php 
+echo "While Loops in PHP";
+echo "<br>";
+// echo 1;
+// echo "<br>";
+// echo 2;
+// echo "<br>";
+// echo 3;
+// echo "<br>";
+// echo 4;
+// echo "<br>";
+// echo 5;
+// echo "<br>";
+
+//  $i =0;
+//  while ($i<5){
+//  echo $i;
+//  echo "<br>";
+// $i++;
+//  }
+
+//  $i =0;
+//  while ($i<5){
+//  echo $i+1;
+//  echo "<br>";
+// $i++;
+//  }
+
+// $i =0;
+// while ($i<500) {
+// echo $i+1;
+// echo "<br>";
+// $i++;
+// }
+
+// $i =0;
+// while ($i<500) {
+// echo $i+1;
+// echo "<br>";
+// $i+=2;
+//  }
+
+//  $i =0;
+// while ($i<500) {
+// echo "The value of i is ";
+// echo $i+1;
+// echo "<br>";
+// $i+=3;
+// }
+
+$i =0;
+ $x = 9;
+while ($i<500 && $x<60) {
+echo "The value of i is ";
+echo $i+1;
+echo "<br>";
+$i+=3;
+$x+=4;
+}
+
+?>
