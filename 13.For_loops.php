5f6d8b88116049e649418ce1babcd8ca5be1ee3b
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-07-31 10:03:17 +0530

    Add files via upload

---
+++ 13.For_loops.php
@@ -0,0 +1,18 @@
+<?php
+echo "This is For Loop in Action <br>";
+// for ($index=0; $index < 5; $index++) { 
+//     # code...
+//     echo "The Number is $index <br>";
+// }
+for ($index=01; $index < 6; $index++) { 
+    # code...
+    echo "The Number is $index <br>";
+}
+
+// for ($i=0; $i < 20;) { 
+//     # code...
+//     echo $i;
+// }
+
+echo "For loop has ended";
+?>
