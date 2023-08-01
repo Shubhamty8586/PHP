461f56365a296be9f6a8851a753a8021bf272442
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-08-01 10:09:18 +0530

    Add files via upload

---
+++ 14.Do_While_loops.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome to Do While Loops <br>";
+/*do{
+Some lines of Code to be Executed here;
+...
+}
+while(Condition);
+*/
+$i= 0;
+do {
+    echo "$i <br>";
+    $i++;
+}
+while ($i<5);
+?>
