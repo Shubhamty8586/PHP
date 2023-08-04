7c3b52eced0cd9a88d06d1b9f052fa1bb2c146d2
Shubham Kumar Tyagi <98882978+Shubhamty8586@users.noreply.github.com>
2023-08-04 11:00:54 +0530

    Add files via upload

---
+++ 17.dates.php
@@ -0,0 +1,8 @@
+<?php 
+echo "Welcome to the World Of Dates <br>";
+$d = date("dS F D Y G:i:s A");
+echo "Today is $d <br>";
+
+$year = date("Y");
+echo "Copyright $year | All rights reseved <br>";
+?>
